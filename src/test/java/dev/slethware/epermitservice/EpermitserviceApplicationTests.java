bd46f7562209007253e7fe4ad083051534353092
DannylSyph3r <dopedanny001@gmail.com>
2026-04-02 16:10:09 +0100

    Chore: Init Project Scaffold

---
+++ src/test/java/dev/slethware/epermitservice/EpermitserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.slethware.epermitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpermitserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
