d43fbcde89b35864feaa3c85180baec46d2baa69
DannylSyph3r <dopedanny001@gmail.com>
2026-04-02 19:47:38 +0100

    Feat: Setup Multi Tenancy RLS pillars and Tenant Data Source configs, validation and context

---
+++ src/main/java/dev/slethware/epermitservice/config/DataSourceConfig.java
@@ -0,0 +1,4 @@
+package dev.slethware.epermitservice.config;
+
+public class DataSourceConfig {
+}
---
+++ src/main/java/dev/slethware/epermitservice/security/TenantWhitelist.java
@@ -0,0 +1,4 @@
+package dev.slethware.epermitservice.security;
+
+public class TenantWhitelist {
+}
