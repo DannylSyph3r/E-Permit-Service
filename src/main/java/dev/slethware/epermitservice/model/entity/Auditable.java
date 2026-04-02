e09dca0c41c73e8b5a00144b4c6feab6dd22a51d
DannylSyph3r <dopedanny001@gmail.com>
2026-04-02 16:30:16 +0100

    Feat: Add Auditable Superclass

---
+++ src/main/java/dev/slethware/epermitservice/model/entity/Auditable.java
@@ -0,0 +1,29 @@
+package dev.slethware.epermitservice.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "UUID")
+    private UUID id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+}
